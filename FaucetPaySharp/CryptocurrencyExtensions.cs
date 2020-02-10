84347330c7f4e5fcc9efd28694ac3818666220ab
jeuxjeux20 Y <jeuxjeux20@outlook.fr>
2020-02-10 17:35:09 +0100

    Made CryptocurrencyExtensions actual extension methods

--- FaucetPaySharp/CryptocurrencyExtensions.cs
+++ FaucetPaySharp/CryptocurrencyExtensions.cs
@@ -7,11 +7,11 @@ namespace FaucetPaySharp
         private const string InaccuracyMessage =
             "Using double to store coin values is not recommended. This can lead to eventual inaccuracies. Please use a decimal value instead.";
 
-        public static long ToSatoshi(decimal coinValue) => (long)(coinValue * 100000000m);
+        public static long ToSatoshi(this decimal coinValue) => (long)(coinValue * 100000000m);
 
         [Obsolete(InaccuracyMessage)]
-        public static long ToSatoshi(double coinValue) => (long)(coinValue * 100000000);
+        public static long ToSatoshi(this double coinValue) => (long)(coinValue * 100000000);
 
-        public static decimal ToCoinValue(long satoshiValue) => satoshiValue / 100000000m;
+        public static decimal ToCoinValue(this long satoshiValue) => satoshiValue / 100000000m;
     }
 }
